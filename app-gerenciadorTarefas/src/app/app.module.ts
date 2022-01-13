ba4085fe02fe17377fa91e7cf9a44170e768205b
Jefferson Santos <jeff.debian2016@gmail.com>
2022-01-13 16:33:09 -0300

    Trabalhando com routing.

--- app-gerenciadorTarefas/src/app/app.module.ts
+++ app-gerenciadorTarefas/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppModuleRoutes } from './app-module.routing';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppModuleRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
